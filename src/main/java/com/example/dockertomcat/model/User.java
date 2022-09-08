6ba72cbe1b1af8e265e14d1d057573d9e47757c4
noslav7 <noslav7@gmail.com>
2022-09-08 08:46:12 +0500

    user

--- src/main/java/com/example/dockertomcat/model/User.java
+++ src/main/java/com/example/dockertomcat/model/User.java
@@ -1,20 +1,79 @@
 package com.example.dockertomcat.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
+    @JsonProperty("id")
+    private int id;
+    @JsonProperty("name")
     private String name;
-    private int age;
+    @JsonProperty("username")
+    private String username;
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("address")
+    private Address address;
+    @JsonProperty("phone")
+    private String phone;
+    @JsonProperty("website")
+    private String website;
+    @JsonProperty("company")
+    private Company company;
 
-    public User(String name, int age) {
-        this.name = name;
-        this.age = age;
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("name", name)
+                .append("username", username)
+                .append("email", email)
+                .toString();
     }
+}
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Address {
+    @JsonProperty("street")
+    private String street;
+    @JsonProperty("suite")
+    private String suite;
+    @JsonProperty("city")
+    private String city;
+    @JsonProperty("zipcode")
+    private String zipcode;
+    @JsonProperty("geo")
+    private Geo geo;
+}
 
-    public String getName() {
-        return name;
-    }
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Geo {
+    @JsonProperty("lat")
+    private String lat;
+    @JsonProperty("lng")
+    private String lng;
+}
 
-    public int getAge() {
-        return age;
-    }
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+class Company {
+    @JsonProperty("name")
+    private String name;
+    @JsonProperty("catchPhrase")
+    private String catchPhrase;
+    @JsonProperty("bs")
+    private String bs;
 }
